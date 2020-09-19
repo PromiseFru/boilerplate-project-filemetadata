8c54f51060b37ef1065c0bd67eb705453ba8c957
Promise Fru <promisefru1@gmail.com>
2020-09-19 20:30:42 +0100

    proj complete

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var express = require('express');
+var upload = require("express-fileupload");
 var cors = require('cors');
 
 // require and use "multer"...
 
 var app = express();
 
+app.use(upload());
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -14,9 +16,19 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
-});
+app.post('/api/fileanalyse', (req, res) => {
+    if(!req.files) {
+        res.json({
+            error: "No file uploaded"
+        });
+    }else {
+        res.json({
+            name: req.files.upfile.name,
+            size: req.files.upfile.size,
+            type: req.files.upfile.mimetype
+        });
+    }
+  });
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
